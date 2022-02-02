e8e27dc487f7cae93f7d4d621b48fd56857fac19
Anton <antonxten@gmail.com>
2022-02-02 21:07:33 +0300

    Homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int numA = 10;
+int numB = 35;
+
+if (numA>numB)
+{
+    Console.WriteLine(numA);
+    Console.WriteLine(numB);
+}
+else if (numB>numA);
+{
+    Console.WriteLine(numB);
+    Console.WriteLine(numA);
+}
