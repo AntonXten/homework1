4a0d8afd8896ece3de1f1d43cda5e08042e42e45
Anton <antonxten@gmail.com>
2022-02-02 21:17:25 +0300

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Найти максимальное из трех чисел
+
+int numA = 254;
+int numB = 2581;
+int numC = 1455;
+
+if(numA>numB)
+{
+    Console.WriteLine(numA);
+}
+else if(numB>numC)
+{
+    Console.WriteLine(numB);
+}
+else if(numC>numA)
+{
+    Console.WriteLine(numC);
+}
